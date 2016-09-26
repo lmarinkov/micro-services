2c534a50afa1ee75aca1db4577cf1ae0e34084c2
latchezar.marinkov@cgm.com <latchezar.marinkov@cgm.com>
2016-09-26 12:32:46 +0200

    prepared publish ...

--- pipeline.jenkinsfile.groovy
+++ pipeline.jenkinsfile.groovy
@@ -1,11 +1,16 @@
  // #!groovy
  // the pipeline script used on jenkins ...
  node {
-     stage('Preparation') { // for display purposes
+     stage('Checkout') { // for display purposes
          git 'https://github.com/lmarinkov/micro-services.git'
+         // clean up git locally
+         sh "git clean -f && git reset --hard origin/master"
+         def pom = readMavenPom file: 'pom.xml'
+         def version = pom.version.replace("-SNAPSHOT", ".${currentBuild.number}")
      }
      stage('Build') {
-         bat(/"mvn" -Dmaven.test.failure.ignore clean package/)
+         //bat(/"mvn" -Dmaven.test.failure.ignore clean package/)
+         bat(/"mvn" -DreleaseVersion=${version} -DdevelopmentVersion=${pom.version} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B"/)
      }
  }
  
