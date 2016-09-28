fa360384667d906cf3fb629f2c06537fb611dfb4
lmarinkov <latchezar.marinkov@hotmail.de>
2016-09-28 17:51:05 +0200

    credentials simplified ...

--- pipeline.jenkinsfile.groovy
+++ pipeline.jenkinsfile.groovy
@@ -4,15 +4,7 @@ node {
     def tag
     def version
     stage('Checkout') { // for display purposes
-        scm {
-            git {
-                remote {
-                    github('lmarinkov/https://github.com/lmarinkov/micro-services.git', 'https')
-                    credentials('7f208d5a-2436-426d-9479-748160abc493')
-                }
-            }
-        }
-        git 'https://github.com/lmarinkov/micro-services.git'
+        git credentialsId: '7f208d5a-2436-426d-9479-748160abc493', 'https://github.com/lmarinkov/micro-services.git'
         // clean up git locally
         //bat (/"git clean -f && git reset --hard origin/master"/)
         def pom = readMavenPom file: 'pom.xml'
