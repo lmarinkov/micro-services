5276f7d4555943a88d6ee50ea9c8d9c64e46a490
latchezar.marinkov@cgm.com <latchezar.marinkov@cgm.com>
2016-09-28 13:17:59 +0200

    adjusting the pipeline script ...

--- pipeline.jenkinsfile.groovy
+++ pipeline.jenkinsfile.groovy
@@ -1,16 +1,27 @@
- // #!groovy
- // the pipeline script used on jenkins ...
- node {
-     stage('Checkout') { // for display purposes
-         git 'https://github.com/lmarinkov/micro-services.git'
-         // clean up git locally
-         //bat (/"git clean -f && git reset --hard origin/master"/)
-         def pom = readMavenPom file: 'pom.xml'
-         def version = pom.version.replace("-SNAPSHOT", ".${currentBuild.number}")
-     }
-     stage('Build') {
-         //bat(/"mvn" -Dmaven.test.failure.ignore clean package/)
-         bat(/"mvn" -DreleaseVersion=${version} -DdevelopmentVersion=${pom.version} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B"/)
-     }
- }
+// #!groovy
+// the pipeline script used on jenkins ...
+node {
+    def version
+    stage('Checkout') { // for display purposes
+        git 'https://github.com/lmarinkov/micro-services.git'
+        // clean up git locally
+        //bat (/"git clean -f && git reset --hard origin/master"/)
+        def pom = readMavenPom file: 'pom.xml'
+        version = pom.version.replace("-SNAPSHOT", ".${currentBuild.number}")
+        echo version.toString()
+        echo currentBuild.toString()
+    }
+    stage('Release:'+ version.toString()) {
+        bat(/"mvn" -f service\pom.xml versions:set -DnewVersion=${version}/)
+    }
+    stage('Build') {
+        bat(/"mvn" -f service\pom.xml clean install/)
+    }
+    stage('Run Firewall Tests') {
+        bat(/"mvn" -f consumer\pom.xml clean install/)
+    }
+    stage('Tag the SCM') {
+        bat(/"mvn" deploy scm:tag/)
+    }
+}
  
