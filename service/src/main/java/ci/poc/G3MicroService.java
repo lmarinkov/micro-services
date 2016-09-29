3ab9c4946d10cd293354fcfa2fa9c8d808936c6d
latchezar.marinkov@cgm.com <latchezar.marinkov@cgm.com>
2016-09-29 13:32:24 +0200

    service implementation ...

---
+++ service/src/main/java/ci/poc/G3MicroService.java
@@ -0,0 +1,14 @@
+package ci.poc;
+
+/**
+ * Date of creation:  29.09.2016
+ *
+ * Copyright \(c\) CompuGROUP Software GmbH,
+ * This software is the confidential and proprietary information of
+ * CompuGROUP Software GmbH. You shall not disclose such confidential
+ * information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with CompuGROUP Software GmbH.
+ */
+public interface G3MicroService {
+    int getCount();
+}
---
+++ service/src/main/java/ci/poc/G3MicroServiceImpl.java
@@ -0,0 +1,16 @@
+package ci.poc;
+
+/**
+ * Date of creation: 29.09.2016
+ * <p>
+ * Copyright \(c\) CompuGROUP Software GmbH,
+ * This software is the confidential and proprietary information of
+ * CompuGROUP Software GmbH. You shall not disclose such confidential
+ * information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with CompuGROUP Software GmbH.
+ */
+public class G3MicroServiceImpl implements G3MicroService {
+    public int getCount() {
+        return 100;
+    }
+}
